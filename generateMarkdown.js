f4bd9c00bda4d46b716f4acf0d8b02666d820dca
Derek Meduri <derekmeduri@gmail.com>
2023-07-05 10:12:53 -0400

    adding table of contents function and license selections

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,35 +1,53 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+//function that returns license badge based on which license is passed in
+//if there is no license, we want to return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-    return;
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-brightgreen)`;
   }
   return "";
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+//creating a function that returns the license link
+//if there is no license, we want to return an empty string
 function renderLicenseLink(license) {
   if (license !== "None") {
-    return;
+    return `\n* [License](#license)\n`;
   }
   return "";
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+//Creating a function that returns the license section of README
+//if there is no license, we want to return an empty string
 function renderLicenseSection(license) {
   if (license !== "None") {
-    return;
+    return `## License
+    Licensed under ${license} license.`;
   }
   return "";
 }
 
-// TODO: Create a function to generate markdown for README
+//Creating a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-    ${renderLicenseBadge()}
-    
+    ${renderLicenseBadge(data.license)}
+    ## Table of Contents
+    *[Description](#description)
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[Contact](#contact)
+    *[Contributors](#contributors)
+    *[Testing](#testing)
+    ${renderLicenseLink(data.license)}
+    ## Description
+    ${data.description}
+    ## Installation 
+    ${data.installation}
+    ## Usage
+    ${data.usage}
+    ## Contact
+
+    ## Contributors
+
 `;
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,69 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("generateMarkdown");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Please name your project.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message:
+      "Please decribe the purpose, use and functionality of your project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please ",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select applicable license for your project.",
+    choices: [
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT License",
+      "BSD 2-Clause 'Simplified' License",
+      "BSD 3-Clause 'New' or 'Revised' License",
+      "Boost Software License 1.0",
+      "Creative Commons Zero v1.0 Unniversal",
+      "Eclipse Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "GNU General PUblic License v2.0",
+      "GNU Lesser General Public License v2.1",
+      "Mozilla Public License 2.0",
+      "The Unlicense",
+      "none",
+    ],
+  },
+  {},
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+//write a README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// Function call to initialize app
+//initialize app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Create a README file");
+    writeToFile("", generateMarkdown({}));
+  });
+}
+
+//calling initialize app
 init();
