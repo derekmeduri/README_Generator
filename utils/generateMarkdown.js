e92adad70c4230bb5c0c2c2827a7a794f61498ee
Derek Meduri <derekmeduri@gmail.com>
2023-07-09 15:50:14 -0400

    adding links to table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -34,17 +34,17 @@ function generateMarkdown(data) {
 
     ## Table of Contents
     
-    * [Description](#Description)
+   - * [Description](#Description)
 
-    * [Installation](#Installation)
+   - * [Installation](#Installation)
 
-    * [Usage](#Usage)
+   -  * [Usage](#Usage)
 
-    * [Questions](#Questions)
+   - * [Questions](#Questions)
 
-    * [Contributors](#Contributors)
+   - * [Contributors](#Contributors)
     
-    * [Testing](#Testing)
+   - * [Testing](#Testing)
     ${renderLicenseLink(data.license)}
     
     ## Description
